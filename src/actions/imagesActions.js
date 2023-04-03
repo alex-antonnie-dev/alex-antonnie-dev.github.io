b5d808273292b89229c0d5ebdbe8175532c6c6fc
alex_antony <alex.antony@enfintechnologies.com>
2023-04-03 08:08:13 +0530

    api changes

--- src/actions/imagesActions.js
+++ src/actions/imagesActions.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 export const fetchImages = createAsyncThunk(
   'images/fetchImages',
   async (page) => {
-    const response = await axios.get(`http://localhost/practice/dg_workshop/API/CONTENTLISTINGPAGE-PAGE${page}.json`);
+    const response = await axios.get(`https://karthikacreations.in/assets/CONTENTLISTINGPAGE-PAGE${page}.json`);
     console.log(response.data.page['content-items'].content)
     return response.data.page['content-items'].content;
   }
