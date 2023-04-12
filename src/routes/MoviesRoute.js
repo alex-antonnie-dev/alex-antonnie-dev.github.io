a485bfe35df0717e61d0bf1cfe485ee197f067b3
alex_antony <alex.antony@enfintechnologies.com>
2023-04-12 22:48:52 +0530

    router fix replacing browserRoute with HashRouter

--- src/routes/MoviesRoute.js
+++ src/routes/MoviesRoute.js
@@ -1,24 +1,16 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import ImageGrid from "../components/ImageGrid";
 import Home from "../components/Home";
 import PageNotFound from "../components/PageNotFound";
 
 function MoviesRoute(){
-  
-  const pathname = window.location.pathname;
-  let basename = process.env.REACT_APP_BASENAME || '';
-  const matched = pathname.match(/^\/[^/]+\/student-dashboard/);
-  if (matched) {
-    basename = matched.toString();
-  }
-  console.log('basename', basename);
 
 return(
-    <Router basename={basename}>
+    <Router>
       <Routes>
         <Route index element={<ImageGrid />} />
         <Route path="/" element={<ImageGrid/>} />
-        <Route path="home" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </Router>
